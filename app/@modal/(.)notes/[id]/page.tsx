72eddcfcc20883fe85dafb2666820e90de6ad949
alina <alina1998hryhorenko@gmail.com>
2026-04-28 00:35:43 +0200

    fix: correct NotePreview file location

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import Modal from "@/components/Modal/Modal";
 import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
+import { useRouter } from "next/navigation";
 
 export default function ModalNotePage() {
   const router = useRouter();
